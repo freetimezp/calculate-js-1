5437389e937ce7102f7d9e5e4763403a07afcf93
freetimezp <kraftukrnet@ukr.net>
2020-08-30 19:57:26 +0300

    Js, calculate. done.

--- src/js/script.js
+++ src/js/script.js
@@ -5,36 +5,75 @@ $(document).ready(function(){
 
 	$('.form-button').on('click', function() {
 		$('.form-popup').addClass('active');
-		selectL.onchange = function() {
 
-		};
-		selectW.onchange = function() {
+		// selectL.onchange = function() {
 
-		};
+		// };
+		// selectW.onchange = function() {
+
+		// };
+		
 		let l = selectL.value;
     let w = selectW.value;
     let area = l * w;
 
-	  document.getElementById("sl").innerHTML = l;
-	  document.getElementById("sw").innerHTML = w;
-	  document.getElementById("area").innerHTML = area;
-
-	  let needPacks = area / 4;
+    let needPacks = area / 4;
 	  let resultEight = Math.floor(needPacks / 8);
 	  let resultSix = Math.floor(needPacks / 6);
 
-	  let a = needPacks % 8;
-	  let b = needPacks % 6;
+	  document.getElementById("sl").innerHTML = l;
+	  document.getElementById("sw").innerHTML = w;
+	  document.getElementById("area").innerHTML = area;
+	  document.getElementById("needPacks").innerHTML = needPacks;
 
-	  if( a == 0 || b == 0 ) {
+
+	  let a = needPacks % 8;
+	  let aE = needPacks / 8;
+
+	  let b = needPacks % 6;
+	  let bS = needPacks / 6;
+
+	  let i = 0;
+	  let y = 0;
+
+	  if ( needPacks <= 6 ) {
+	  	document.getElementById("packsSix").innerHTML = 1;
+	  	document.getElementById("packsEight").innerHTML = 0;
+	  } else if ( a == 0 && resultEight == aE ) {
 	  	document.getElementById("packsSix").innerHTML = 0;
 	  	document.getElementById("packsEight").innerHTML = resultEight;
-	  } 
+	  } else if ( b == 0 ) {
+	  		i = needPacks;
+	  		do {
+	  			i = i - 6;
+	  			y = y + 1;
+				} while ( i % 8 !== 0 );
+	  		document.getElementById("packsSix").innerHTML = y ;
+	  		document.getElementById("packsEight").innerHTML = i / 8;
+	  } else if ( needPacks % 2 == 1 ) {
+	  		i = needPacks + 1;
+	  		do {
+	  			i = i - 6;
+	  			y = y + 1;
+				} while ( i % 8 !== 0 );
+	  		document.getElementById("packsSix").innerHTML = y ;
+	  		document.getElementById("packsEight").innerHTML = i / 8;
+	  } else if ( a > 0 || b > 0 ) {
+	  		i = needPacks;
+	  		do {
+	  			i = i - 6;
+	  			y = y + 1;
+				} while ( i % 8 !== 0 );
+	  		document.getElementById("packsSix").innerHTML = y ;
+	  		document.getElementById("packsEight").innerHTML = i / 8;
+	  }
 
 	});
 
 	$('.popup-button').on('click', function() {
 		$('.form-popup').removeClass('active');
+			i = 0;
+	 	  y = 0;
 	});
 
 });
